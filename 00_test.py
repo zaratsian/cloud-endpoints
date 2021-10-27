b475a1a74c84266af2e732209e4fee0a8eed83ff
Dan Zaratsian <dtz001@gmail.com>
2021-10-27 14:43:56 +0000

    cleanup

--- 00_test.py
+++
@@ -1,4 +0,0 @@
-import requests
-import subprocess 
-
-subprocess.run(["gcloud", "run", "services", "list", "--format", '"value(status.url)"', "--filter", '"metadata.name=$APP_CLOUD_RUN_NAME"'])
--- deploy_cloud_run.sh
+++
@@ -1,9 +0,0 @@
-# Load Configs
-. ./config
-
-gcloud run deploy $CLOUD_RUN_NAME \
-    --source . \
-    --allow-unauthenticated \
-    --platform managed \
-    --region $GCP_REGION \
-    --project=$GCP_PROJECT_ID
